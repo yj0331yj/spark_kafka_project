31331291a3ba5f46db155dd35b197805b3c9ac0f
yoonjin <yj0331yj@gmail.com>
2024-09-20 01:15:08 +0900

    upload

---
+++ producer/kakao_book_api.py
@@ -0,0 +1,59 @@
+import requests
+import json
+
+# 카프카 프로듀서를 통해 데이터를 전송하기 위한 라이브러리
+from confluent_kafka import Producer
+
+import proto.book_data_pb2 as pb2
+class KakaoException(Exception):
+    pass
+
+def get_original_data(query: str) -> dict:
+    rest_api_key = "2581882b8ab7e2c66d02a981b483d26c"
+    url = "https://dapi.kakao.com/v3/search/book"
+    # requests.get() 메소드를 통해 res(=response) 응답 개체를 받아옴
+    res = requests.get(
+        url=url,
+        headers={
+            "Authorization": f"KakaoAK {rest_api_key}",
+        },
+        params={
+            "query": query,
+            "sort": 50,
+            "start": 1
+        }
+    )
+    # 에러 처리 : status code가 4 또는 5로 시작하면 클라이언트 에러 또는 서버 에러
+    if res.status_code >= 400:
+        raise KakaoException(res.content)
+
+    # json.loads() : 위 텍스트를 딕셔너리로 바꿔주는 메소드
+    return json.loads(res.text)
+
+if __name__ == '__main__':
+    original_data = get_original_data(query="베스트셀러")
+
+    # kafka configs 세팅
+    conf = {
+        'bootstrap.servers': 'localhost:29092', # docker container 설정 시 로컬에서는 9092가 아닌 29092로 요청을 보내도록 설정함
+    }
+
+    producer = Producer(conf)
+    topic = "book"
+
+    for item in original_data['documents']:
+        book = pb2.Book()
+
+        book.title = item['title']
+        book.author = '.'.join(item['authors']) # authors의 kakao json 형식은 list로 되어 있음 -> proto로 지정한 string 형식으로 변환 설정
+        book.publisher = item['publisher']
+        book.isbn = item['isbn']
+        book.price = int(item['price'])
+        book.publication_date = item['datetime']
+        book.source = 'kakao'
+        print("----")
+        print(book)
+        print("----")
+        producer.produce(topic=topic, value=book.SerializeToString()) # book은 protocol buffer 형태 이므로 string으로 변환해야 함
+        producer.flush() # 한 번에 많은 양의 데이터를 보내면 producer가 terminating되므로 flush를 사용해서 지연시킴(기다리게 함)
+        print("전송 완료")
---
+++ producer/naver_book_api.py
@@ -0,0 +1,59 @@
+import requests
+import json
+
+from confluent_kafka import Producer
+import proto.book_data_pb2 as pb2
+
+class NaverException(Exception):
+    pass
+
+def get_original_data(query: str) -> dict:
+    client_id = "6yxU63bUcV180HWR4V3Y"
+    client_secret = "TzF80YJekZ"
+    url = "https://openapi.naver.com/v1/search/book.json"
+    # requests.get() 메소드를 통해 res(=response) 응답 개체를 받아옴
+    res = requests.get(
+        url=url,
+        headers={
+            "X-Naver-Client-Id": client_id,
+            "X-Naver-Client-Secret": client_secret
+        },
+        params={
+            "query": query,
+            "display": 100,
+            "start": 1,
+        }
+    )
+    # 에러 처리 : status code가 4 또는 5로 시작하면 클라이언트 에러 또는 서버 에러
+    if res.status_code >= 400:
+        raise NaverException(res.content)
+
+    # json.loads() : 위 텍스트를 딕셔너리로 바꿔주는 메소드
+    return json.loads(res.text)
+
+if __name__ == '__main__':
+    original_data = get_original_data(query="베스트셀러")
+
+    # kafka configs 세팅
+    conf = {
+        'bootstrap.servers': 'localhost:29092',
+    }
+    producer = Producer(conf)
+    topic = "book"
+
+    for item in original_data['items']:
+        book = pb2.Book()
+        # dictionary -> protobuf
+        book.title = item['title']
+        book.author = item['author']
+        book.publisher = item['publisher']
+        book.isbn = item['isbn']
+        book.price = int(item['discount'])
+        book.publication_date = item['pubdate']
+        book.source = 'naver'
+        print("----")
+        print(book)
+        print("----")
+        producer.produce(topic= topic, value=book.SerializeToString())
+        producer.flush()
+        print("전송 완료")
